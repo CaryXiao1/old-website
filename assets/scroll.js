250526e9cff8243106b82eec20db9a9dae93e0bc
Cary Xiao <caryxiao@stanford.edu>
2021-12-16 18:57:25 -0600

    add scroll animation

--- assets/scroll.js
+++ assets/scroll.js
@@ -1,57 +1,64 @@
 
-$(document).ready( function () { // TODO: copied code from window.onresize; potentially think of less line-code
-  // corrects part of screen that user should be on during window resize
-  // document.body.scrollTop = pageNum * window.innerHeight;
-  // document.documentElement.scrollTop = pageNum * window.innerHeight;
-
-  // resize window if on mobile
-  /*  if(window.matchMedia("(max-width: 767px)").matches){
-    // The viewport is less than 768 pixels wide
-    alert("This is a mobile device.");
-    window.visualViewport
-  } */
-  
-
+$(document).ready( function () {
   // centers portfolio container
-  let top = window.innerHeight / 2 - (document.getElementById("portfolio-center").clientHeight / 2);
+  let top = window.innerHeight / 2 - (document.getElementById("page-0").clientHeight / 2);
   let left = 0; // window.innerWidth / 2 - (document.getElementById("portfolio-center").clientWidth / 2); 
-  document.getElementById("portfolio-center").style.top = top + "px";
-  document.getElementById("portfolio-center").style.left = left + "px";
+  document.getElementById("page-0").style.top = top + "px";
+  document.getElementById("page-0").style.left = left + "px";
 
-  left =  window.innerWidth / 2 - (document.getElementById("portfolio-center").clientWidth / 2);
-  document.getElementById("portfolio-center").style.left = left + "px";
+  left =  window.innerWidth / 2 - (document.getElementById("page-0").clientWidth / 2);
+  document.getElementById("page-0").style.left = left + "px";
 });
 
 let pageNum = 0;
-// When the user clicks on the button, change page that is shown to user
+// When the user clicks on the button, animate and change page that is shown to user
 function cardUp() {
   if (pageNum > 0) {
-    document.getElementById("page-" + pageNum).setAttribute("hidden", true);
-    pageNum--;
-    document.getElementById("page-" + pageNum).removeAttribute("hidden");
-    document.getElementById("d-arr-wrap").removeAttribute("hidden");
-
-    if (pageNum == 0) {
+    $("#page-" + pageNum).animate({
+      top: '70em'
+    }, 400, function() {
+      document.getElementById("page-" + pageNum).setAttribute("hidden", true);
+      document.getElementById("page-" + pageNum).style.top = 0;
+      pageNum--;
+      var targetNum = (pageNum != 0) ? 0 : window.innerHeight / 2.8 - (document.getElementById("page-0").clientHeight / 2);
+      if (targetNum != 0) 
+      document.getElementById("d-arr-wrap").removeAttribute("hidden");
+      document.getElementById("page-" + pageNum).style.top = '-70em';
+      document.getElementById("page-" + pageNum).removeAttribute("hidden");
+      $('#page-' + pageNum).animate({
+        top: targetNum
+      }, 400)
+    })
+    
+    if (pageNum == 1) {
       document.getElementById("u-arr-wrap").setAttribute("hidden", true);
     }
   }
 }
+
 function cardDown() {
   if (pageNum < 2) {
-    document.getElementById("page-" + pageNum).setAttribute("hidden", true);
-    pageNum++;
-    document.getElementById("page-" + pageNum).removeAttribute("hidden");
-    document.getElementById("u-arr-wrap").removeAttribute("hidden");
+    $("#page-" + pageNum).animate({
+      top: '-70em'
+      }, 400, function() {
+      document.getElementById("page-" + pageNum).setAttribute("hidden", true);
+      document.getElementById("page-" + pageNum).style.top = 0;
+      pageNum++;
+      document.getElementById("u-arr-wrap").removeAttribute("hidden");
+      document.getElementById("page-" + pageNum).removeAttribute("hidden");
+      document.getElementById("page-" + pageNum).style.top = '70em';
 
-    if (pageNum == 2) {
+      $('#page-' + pageNum).animate({
+        top: 0
+      }, 400)
+    })
+    // alert(pageNum);
+    if (pageNum == 1) {
       document.getElementById("d-arr-wrap").setAttribute("hidden", true);
     }
   }
-
-  
-  //document.body.scrollTop += window.innerHeight;
-  //document.documentElement.scrollTop += window.innerHeight;
 }
+
 $(document).keydown(function(e) {
   switch (e.which) {
     case 38: //up arrow key
@@ -65,16 +72,12 @@ $(document).keydown(function(e) {
 });
 
 window.onresize = function() {
-  // corrects part of screen that user should be on during window resize
-  document.body.scrollTop = pageNum * window.innerHeight;
-  document.documentElement.scrollTop = pageNum * window.innerHeight;
-
   // centers portfolio container
-  document.getElementById("portfolio-center").style.left = "0" + "px";
-  var top = window.innerHeight / 2 - (document.getElementById("portfolio-center").clientHeight / 2);
-  var left = window.innerWidth / 2 - (document.getElementById("portfolio-center").clientWidth / 2); 
-  document.getElementById("portfolio-center").style.top = top + "px";
-  document.getElementById("portfolio-center").style.left = left + "px";
-  document.getElementById("portfolio-center").removeAttribute("hidden");
+  document.getElementById("page-0").style.left = "0" + "px";
+  var top = window.innerHeight / 2 - (document.getElementById("page-0").clientHeight / 2);
+  var left = window.innerWidth / 2 - (document.getElementById("page-0").clientWidth / 2); 
+  document.getElementById("page-0").style.top = top + "px";
+  document.getElementById("page-0").style.left = left + "px";
+  document.getElementById("page-0").removeAttribute("hidden");
   
 };
